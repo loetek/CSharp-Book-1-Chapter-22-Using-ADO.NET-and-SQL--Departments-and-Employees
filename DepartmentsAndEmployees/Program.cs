59925d0da14989efd6d0aaae6dbe5ebfad0b4db0
loetek <jdwheeler0@gmail.com>
2019-03-18 15:29:45 -0500

    Add project files.

---
+++ DepartmentsAndEmployees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DepartmentsAndEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
